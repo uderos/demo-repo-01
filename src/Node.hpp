4ae51065a6400eee67e8699122e3db6b09322a99
U.DeRos <umberto@exa.com>
2015-03-09 16:06:05 -0400

    The big mess (ws03)

--- src/Node.hpp
+++ src/Node.hpp
@@ -8,42 +8,6 @@
 
 class Node
 {
-  public:
-
-    enum eFlags
-    {
-      PROCESSED,
-      VISITED,
-
-      NUM_FLAGS
-    };
-
-    typedef std::set<Node *>node_set_t;
-
-    Node(const std::string & name);
-    virtual ~Node();
-
-    const std::string & GetName() const;
-    const node_set_t & GetInputNodes() const;
-    const node_set_t & GetOutputNodes() const;
-
-    void AddInputNode(Node & input_node);
-    void AddOutputNode(Node & output_node);
-
-    void SetFlag(const eFlags flag);
-    void ClearFlag(const eFlags flag);
-    bool HasFlag(const eFlags flag) const;
-    
-    void Dump(std::ostream & oss) const;
-
-  private:
-    std::string m_name;
-
-    node_set_t m_input_nodes;
-    node_set_t m_output_nodes;
-
-    std::bitset<NUM_FLAGS> m_flags;
-
 };
 
 
--- src/NodeGenerator.hpp
+++ src/NodeGenerator.hpp
@@ -9,28 +9,6 @@
 
 class NodeGenerator
 {
-  public:
-    NodeGenerator();
-    virtual ~NodeGenerator();
-
-    bool LoadFile(const std::string & fname);
-
-    Node::node_set_t GetNodes() const;
-
-    void Dump() const;
-
-
-  private:
-
-    typedef std::map<std::string, Node *> node_map_t;
-    node_map_t m_nodes;
-
-    Node * m_get_node(const std::string & name);
-    bool m_add_link(const std::string & from_node_name, 
-                    const std::string & to_node_name);
-                
-    bool m_load_nodes(std::ifstream & ifs);
-    bool m_load_links(std::ifstream & ifs);
 };
 
 
